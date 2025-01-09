ce96591e2927642cd8c53fe53f2593163e01ee13
yank machado <knaygit@outlook.com>
2025-01-09 20:44:44 -0300

    add a new view and URL pattern for 'sobre' page

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def my_view(): 
+    return HttpResponse('Olá, mundo!')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
